8de6cfda4798529fc928329dfd884688c6cd5cd7
Drappear <44987855+Drappear@users.noreply.github.com>
2022-05-06 17:04:19 +0900

    constructor, autowired

---
+++ src/main/java/lecture/c09core/Wheel.java
@@ -0,0 +1,12 @@
+package lecture.c09core;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Wheel {
+
+	public void roll() {
+		System.out.println("rolling wheel");
+	}
+
+}

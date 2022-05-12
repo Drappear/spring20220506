393a5dc56d471e45c9f8ac7e0ba2021f0d99af5f
Drappear <44987855+Drappear@users.noreply.github.com>
2022-05-12 17:34:48 +0900

    dto

--- src/main/java/org/zerock/domain/ex01/EmployeeDto.java
+++ src/main/java/org/zerock/domain/ex01/EmployeeDto.java
@@ -6,9 +6,13 @@ import lombok.Data;
 
 @Data
 public class EmployeeDto {
+	private int id;
 	private String firstName;
 	private String lastName;
-	private Date birthDate;
+//	@DateTimeFormat(pattern = "yyyy-MM-dd") -- 둘 중 하나
+//	@DateTimeFormat(iso = ISO.DATE)			-- 둘 중 하나(국제 표준)
+//	private LocalDate birthDate;
+	private Date birthDate;		// Date 타입은 siple value type목록에 있음
 	private String photo;
 	private String notes;		
 	
